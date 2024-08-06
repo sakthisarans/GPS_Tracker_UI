ce4c803417ea7d9b8252adb0f907eaac38a228f4
sakthisaranprjx <sakthisaransss55@gmail.com>
2024-08-06 22:03:35 +0530

    updated not found

--- src/pages/error/NotFound.tsx
+++ src/pages/error/NotFound.tsx
@@ -1,10 +1,21 @@
 import { Fragment } from "react/jsx-runtime";
 import "./NotFound.css"
+import { useNavigate } from "react-router-dom";
 
 function NotFound(): JSX.Element {
+    const navigate=useNavigate()
     return (
         <Fragment>
-            <p>not found</p>
+            <div id="notfound">
+                <div className="notfound">
+                    <div className="notfound-404">
+                        <h1>404</h1>
+                        <h2>Page not found</h2>
+                    </div>
+                    <button onClick={()=>navigate('/home',{ replace: true })}>Homepage</button>
+                    
+                </div>
+            </div>
         </Fragment>
     )
 }
