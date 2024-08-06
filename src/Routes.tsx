9090edf8448735be8d5b3e5e20395ecdd16ccc87
sakthisaranprjx <sakthisaransss55@gmail.com>
2024-08-06 18:58:06 +0530

    Update Routes.tsx

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,61 +4,70 @@ import LoginPage from "./pages/loginSignup/LoginPage";
 import AboutUs from "./pages/AboutUs/AboutUs";
 import axios from "axios";
 
-const authenticate = async () =>{
-  console.log(process.env.REACT_APP_.BASE_URL)
-    const token = localStorage.getItem("Token")
-    const validateToken = async ():Promise<boolean> => {
-      const res=await axios.get(`${process.env.REACT_APP_.BASE_URL}/tracker/user/validateToken`)
-      console.log("res")
-      return (res && res.status===200);
-      
-    }
-    if (!token && await validateToken()) {
+const authenticate =  async () => {
+  console.log(process.env.REACT_APP_BASE_URL)
+  const token = localStorage.getItem("Token")
+  const validateToken = async (): Promise<boolean> => {
+    const res = await axios.get(`${process.env.REACT_APP_BASE_URL}/tracker/user/validateToken`, {
+      headers: {
+        "Authorization": token
+      }
+    })
+    return (res && res.status === 200);
+
+  }
+  if (!token) {
+    delete axios.defaults.headers.common["Authorization"];
+    localStorage.removeItem("Token");
+    return <Navigate to="/login" />;
+  } else {
+    if (await validateToken()) {
+      axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+      return <Outlet />;
+    } else {
       delete axios.defaults.headers.common["Authorization"];
       localStorage.removeItem("Token");
       return <Navigate to="/login" />;
-    }else{
-      axios.defaults.headers.common["Authorization"] = "Bearer " + token;
-      return <Outlet />;
     }
+  }
 }
-const Routes = () =>{
-    const routesForPublic = [
-        {
-          path: "/login",
-          element: <LoginPage />,
-        },
-        {
-          path: "/aboutus",
-          element: <AboutUs />,
-        },
-      ];
+const Routes = () => {
+  const routesForPublic = [
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+    {
+      path: "/aboutus",
+      element: <AboutUs />,
+    },
+  ];
 
-      const routesForAuthenticatedOnly = [
+  const routesForAuthenticatedOnly = [
+    {
+      path: "/",
+      element: authenticate(),
+      children: [
         {
-          path: "/",
-          element: authenticate(), 
-          children: [
-            {
-              path: "/home",
-              element: <div>User Home Page</div>,
-            },
-            {
-              path: "/profile",
-              element: <div>User Profile</div>,
-            },
-            {
-              path: "/logout",
-              element: <div>logout Profile</div>,
-            },
-          ],
+          path: "/home",
+          element: <div>User Home Page</div>,
         },
-      ];
-      const router = createBrowserRouter([
-        ...routesForPublic,
-        ...routesForAuthenticatedOnly,
-      ]);
-      return <RouterProvider router={router} />;
+        {
+          path: "/profile",
+          element: <div>User Profile</div>,
+        },
+        {
+          path: "/logout",
+          element: <div>logout Profile</div>,
+        },
+      ],
+    },
+  ];
+  const router = createBrowserRouter([
+    ...routesForPublic,
+    ...routesForAuthenticatedOnly,
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default Routes;
