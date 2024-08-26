60b4d54a6c8adbc16e65cc2707cdb76a5715233b
sakthisaranprjx <sakthisaransss55@gmail.com>
2024-08-26 17:31:42 +0530

    updated map component

--- src/components/Home/MapComponent.tsx
+++ src/components/Home/MapComponent.tsx
@@ -1,34 +1,44 @@
 import { Fragment } from "react/jsx-runtime";
 import '@tomtom-international/web-sdk-maps/dist/maps.css'
 import tt from '@tomtom-international/web-sdk-maps';
-import { useEffect, useRef, useState } from "react";
+import { createRef, useEffect, useState } from "react";
+import "./MapComponent.css"
 
-function MapComponent () {
-    const mapElement = useRef<HTMLDivElement>(null);
-    const [mapLongitude, setMapLongitude] = useState(-21.91599);
-    const [mapLatitude, setMapLatitude] = useState(37.36765);
-    const [mapZoom, setMapZoom] = useState(6);
-    const [maps, setMap] = useState({});
-    useEffect(() => {
-        let map = tt.map({
-          key: `${process.env.REACT_APP_TOMTOM_KEY}`,
-          container: mapElement.current??"",
-          // center: [37.36765,-21.91599],
-          // zoom: 13
-          center: [mapLongitude, mapLatitude],
-          zoom: mapZoom
-        });
+function MapComponent() {
+  const mapElement = createRef<HTMLDivElement>();
+  const [mapLongitude, ] = useState(80.2705);
+  const [mapLatitude, ] = useState(13.0843);
+  const [, setMap] = useState({});
+
+  useEffect(() => {
+     
+      let map = tt.map({
+        key: `${process.env.REACT_APP_TOMTOM_KEY}`,
+        container: mapElement.current ?? "mapDiv",
+        center: [mapLongitude, mapLatitude],
+        zoom: 13,
+      });
+
+      var element = document.createElement("div")
+      element.id = "marker"
+      var marker = new tt.Marker({ element:element }).setLngLat([mapLongitude, mapLatitude]).addTo(map)
+      var popup = new tt.Popup().setHTML(
+        "<b>TN 15 X 1234</b>"
+      )
+      marker.setPopup(popup).togglePopup()
+      setMap(map);
+      return () => map.remove();
     
-        setMap(map);
-        return () => map.remove();
-      }, [mapLatitude,mapLongitude,mapZoom]);
-    return(
-        <Fragment>
-            <div className="map">
-            <div ref={mapElement} className="map" id="map"/>
-            </div>
-        </Fragment>
-    )
+
+  }, [mapLatitude, mapLongitude]);
+
+  return (
+    <Fragment>
+      <div className="mapDiv">
+        <div ref={mapElement} className="mapDiv" id="map" />
+      </div>
+    </Fragment>
+  )
 }
 
 export default MapComponent;
